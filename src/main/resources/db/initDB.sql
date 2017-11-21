cde9955193de3d1659b4ca76825530ca71d712c4
khomiakAndriy <khomiak.andriy@gmail.com>
2017-11-21 15:39:16 +0200

    create and populate db

---
+++ src/main/resources/db/initDB.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+  id               INT(10) NOT NULL AUTO_INCREMENT,
+  name             VARCHAR(100)                  NOT NULL,
+  email            VARCHAR(100)                 NOT NULL,
+  password         VARCHAR(100)                 NOT NULL,
+  registered       TIMESTAMP DEFAULT now() NOT NULL,
+  enabled          BOOL DEFAULT TRUE       NOT NULL,
+  calories_per_day INTEGER DEFAULT 2000    NOT NULL,
+  PRIMARY KEY (id)
+)
+AUTO_INCREMENT = 100000;
+
+CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+
+CREATE TABLE user_roles
+(
+  user_id INTEGER(10) NOT NULL,
+  role    VARCHAR(100),
+  CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
