f9f79967480542f861fb8233be2386faaa098d5b
khomiakAndriy <khomiak.andriy@gmail.com>
2017-12-19 14:43:22 +0200

    add ActiveProfilesResolver ?

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -12,4 +12,18 @@ public class Profiles {
             HSQL_DB = "hsqldb";
 
     public static final String ACTIVE_DB = POSTGRES_DB;
+    //  Get DB profile depending of DB driver in classpath
+    public static String getActiveDbProfile() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return POSTGRES_DB;
+        } catch (ClassNotFoundException ex) {
+            try {
+                Class.forName("org.hsqldb.jdbcDriver");
+                return Profiles.HSQL_DB;
+            } catch (ClassNotFoundException e) {
+                throw new IllegalStateException("Could not find DB driver");
+            }
+        }
+    }
 }
